a3510baa6f5f168c2138e56844f136486fb5ee79
Micaela Palomeque <micaelapalomeque07@gmail.com>
2026-04-26 23:11:19 -0300

    Agrego gitignore y uso variables de entorno

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:coca2025@localhost:5432/peluqueria_api"
+DATABASE_URL =  os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
